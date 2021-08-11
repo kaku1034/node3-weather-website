f8dd86e4bc9aa074d7e25296103f8889eef0c827
KAKU <kakuda0201@gmail.com>
2021-08-11 16:48:47 +0900

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=a26ee778e3ffc700b15f5c9d04536397&query=${latitude},${longitude}&units=f`
+    request({ url, json: true}, (error, { body }) => {
+        if(error) {
+            callback('Unable to connect to weather server', undefined)
+        } else if(body.error) {
+            callback('Unable to find location', undefined)
+        } else{
+            callback(undefined, `${body.current.weather_descriptions[0]} It is currentry ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degress out.`)
+        }
+    })
+}
+
+module.exports = forecast
